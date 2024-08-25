b5e27674b7230a076f2e03c3883c2c5c3bf44413
Samuel <samuelkumar721@gmail.com>
2024-08-25 11:50:20 +0530

    Add new Contact

---
+++ frontend/src/container/ChatMember.jsx
@@ -0,0 +1,116 @@
+import React, { useState, useEffect } from 'react'
+import add from '../image/add.png'
+
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom'
+import more from '../image/more.png';
+
+const ChatMember = ({userData, setChatSelect, setSelectedUser,setSelectedUserImage }) => {
+  const navigate = useNavigate();
+
+  const [userList,setUserList]= useState([])
+
+ 
+  const handleClick = () => {
+
+   
+   
+    navigate(`/admin/${userData._id}/newContact`,{state:{userData:userData}});
+    // // Scroll to the form
+    // if (formRef.current) {
+    //   formRef.current.scrollIntoView({ behavior: 'smooth' });
+    // }
+  }
+  // Post NewContact data
+
+  useEffect(() => {
+    fetchData()
+    }, []);
+    
+    const fetchData= async () => {
+      try {
+        const response = await axios.get('http://localhost:8080/newuser');
+
+        setUserList(response.data);
+        console.log(response.data);
+  
+      } catch (error) {
+        console.error('Error fetching names:', error);
+      }
+    }
+
+    const handleLogout=()=>{
+      navigate("/login");
+  }
+    
+  return (
+    <div>
+      <div>
+        <div className='m-1 sticky top-0'>
+          <div className='mx-3 flex justify-between'>
+            <span className=' text-2xl text-black font-bold'>{userData.name} 
+              <span className='text-xl text-black font-bold'>({userData.mobile})</span></span>
+          <div className='flex gap-3'>
+              {/* AddButton */}
+              <img className='w-8 h-8 bg-white rounded-full cursor-pointer  transition-transform duration-300 hover:scale-125' onClick={handleClick} src={add} alt="" />
+         {/* Logout */}
+         <button onClick={handleLogout} className='md:hidden  w-fit px-3  py-1 bg-red-500 hover:bg-red:300 text-white font-semibold rounded-full  transition-transform duration-300 hover:scale-130'>
+         Logout</button>
+          </div>
+          </div>
+        </div>
+        <div className='m-3'>
+          <input className=' p-2 w-full bg-slate-200 rounded-xl' type="text" placeholder='Enter to search' />
+
+        </div>
+        <div className='ml-3 flex  gap-2'>
+          <button className='bg-slate-300 py-2 px-4 rounded-full'>All</button>
+          <button className='bg-slate-300 py-2 px-3 rounded-full'>Unseen</button>
+          <button className='bg-slate-300 py-2 px-3 rounded-full'>Group</button>
+        </div>
+      </div>
+      <div className='p-4 h-[70vh] overflow-y-scroll '>
+
+        {/* All Contact */}
+
+        {userList.map((user) => (
+        
+            <div className='flex justify-between mb-3 pb-1 w-full cursor-pointer  border-b-2 hover:bg-slate-300' key={user._id} onClick={() => 
+              {setChatSelect(true)
+              setSelectedUser(user.name)
+              setSelectedUserImage(user.image)
+            }
+
+            }>
+             <div className='flex ' >
+              <img className='w-12 h-12 rounded-full ' src={user.image} alt='' >
+              
+              </img>
+              
+              <div className='ml-3 w-full '>
+                <span>{user.name}</span>
+                <p> {user.mobile}</p>
+              </div>
+              </div>
+             <div className=''  >
+             <button className='ml-3   hover:bg-slate-100 hover:rounded-full hover:text-white'>
+                <img className="w-8 h-8" src={more} alt="" />
+              </button>
+          
+         
+            </div>
+            
+            </div>
+        ))}
+      </div>
+
+
+      {/* AddUser */}
+
+    
+    </div>
+
+  )
+}
+
+export default ChatMember
---
+++ frontend/src/container/NewContact.jsx
@@ -0,0 +1,94 @@
+import React, { useState } from 'react'
+import axios from 'axios';
+import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom'
+import { useLocation } from 'react-router-dom';
+
+
+function NewContact() {
+    
+    const navigate = useNavigate()
+    const location = useLocation()
+    const { userData } = location.state || {};
+
+    const [name,setName] = useState("");
+    const [image,setImage] = useState("");
+    const [mobile,setMobile] = useState("");
+    const [error,setError] =useState("")
+
+    const {register,
+        handleSubmit,
+        formState:{errors}
+    } = useForm();
+   
+    const Submit=async(data)=>{
+        await axios.post("http://localhost:8080/newuser",{name:name,image:image,mobile:mobile})
+         .then((response)=>{
+
+            setName("")
+            setImage("")
+            setMobile("")
+            console.log(response);
+
+            //Navigate to HomePage
+            console.log(userData);
+
+            navigate(`/admin/${userData._id}`,{state:{ userData: userData }})
+           
+         })
+         .catch((error)=>{
+           
+            if(error?.response?.status===409){
+                setError("Number Exist, Please use another number");
+            }
+            else{
+             setError(error.message);
+             }
+          
+         }) 
+   
+    }
+
+      
+    return (
+
+        <div className='w-[100vw] h-full'>
+
+            <form  onSubmit={handleSubmit(Submit)}  >
+
+                <div class="row" >
+                    <div class="col-8 offset-2">
+                        <span class="text-3xl offset-2 font-semibold">Add new whatsapp user</span>
+                        <div class="components">
+                            <span>Username:</span>
+                            <input class="form-control" type="text" placeholder="Enter  username." {...register("name",{required:true, minLength:{value:3,message:"Minimum 3 characters required"},maxLength:{value:15,message:"characters exceeded "}}) } 
+                          value={name} onChange={(e)=>{setName(e.target.value)}} />
+                            {errors.name && <p className='text-red-700'>{errors.name.message}</p>}
+                        </div>
+                        
+                        <div class="components">
+                            <span>Image:</span>
+                            <input class="form-control" type="text" placeholder="image link" value={image} {...register("image")} onChange={(e)=>{setImage(e.target.value)}} />
+                        </div>
+        <div class="components">
+                            <span>Mobile:</span>
+                            <input class="form-control" type="text" placeholder="Enter your Mobile No." {...register("mobile",{required:true, minLength:{value:10,message:"Minimum 10 characters required"},
+                              maxLength:{value:10,message:"characters exceeded "}})} 
+                            value={mobile} onChange={(e)=>{setMobile(e.target.value)}} required="true"/>
+                            {errors.mobile && <p className='text-red-700'>{errors.mobile.message}</p>}
+                        </div>
+
+                        <div class="mt-3">
+                            <button class="btn btn-success" type="submit">Save</button>
+                        </div>
+                        {error && <p style={{ color: 'red' }}>{error}</p>}
+
+                    </div>
+                </div>
+            </form>
+          
+        </div>
+    )
+}
+
+export default NewContact
