f8ae1a266e53406a4a1c6b3aac014e1585d400fd
Samuel <samuelkumar721@gmail.com>
2024-09-11 15:20:19 +0530

    Communication building in backend

--- backend/models/whatsappMessage.js
+++ backend/models/whatsappMessage.js
@@ -1,18 +1,13 @@
 const mongoose = require("mongoose");
 const Schema  = mongoose.Schema;
 
-const whatsappMessageSchema = new Schema({
-    message:{
-        type:String,
-       
-    },
-    sendByMe:{
-        type:String,
+const MessageSchema = new mongoose.Schema({
+    senderId: Schema.Types.ObjectId,
 
-    },
-    time:{
-        type:String,
-        required:true,
-    }       
-  })
-  module.exports = mongoose.model("whatsappMessage",whatsappMessageSchema)
+    receiverId: Schema.Types.ObjectId,
+
+    message: String,
+    timestamp: { type: Date, default: Date.now },
+});
+
+  module.exports = mongoose.model("whatsappMessage",MessageSchema)
--- backend/whatsappServer.js
+++ backend/whatsappServer.js
@@ -13,6 +13,7 @@ const methodOverride = require("method-override");
 
 const cors = require("cors");
 const bodyParser=require("body-parser");
+const whatsappContacts = require("./models/whatsappContacts.js");
 
 
 
@@ -59,75 +60,9 @@ app.get("/",(req,res)=>{
     res.send("Wellcome to Whatsapp admin");
 })
 
-app.get("/newuser",wrapAsync(async(req,res)=>{
-    const users = await whatsappContact.find();
-         res.send(users);
-     // res.render("whatsapp/newUser.ejs");
- }))
-app.post("/newuser",wrapAsync(async(req,res)=>{
-
-    const {name,image,mobile} = req.body;
-    const userExist = await whatsappContact.findOne({mobile:req.body.mobile})
-    if(userExist){
-        console.log("Number existed");
-        res.status(409).send(userExist);
-    }
-   else{
-    const newUser = new whatsappContact({
-        name,
-        image,
-        mobile
-    })
-    await newUser.save();
-    console.log("New contact Added");
-
-   
-    res.send("New contact Added");
-   }
-}))
-
-//signup
-
-// app.post("/signup",async(req,res)=>{
-//     const {name,image,mobile} = req.body;
-
-    
-//     const newAdmin = new whatsappAdmin({
-//         name,image,mobile
-//     })
-//    const newRegistation = await whatsappAdmin.register(newAdmin,password);
-
-//    res.send("New Admin Registered Successfully");
-    
-// })
-//Admin signup
-app.post("/admin/signup",wrapAsync(async(req,res)=>{
-    const {username,mobile,password}= req.body;
-console.log(req.body);
-
-    const userExist = await whatsappAdmin.findOne({mobile:req.body.mobile})
-    if(userExist){
-        res.status(409).send("Number is already used.");
-    }
-    else{
-        const newUser = new whatsappContact({ name:username, mobile })
-        const newAdmin = new whatsappAdmin({
-            username,mobile,allContacts:newUser
-        })
-        //Contact Adding
-       //Passsing ref of Contact
-       
-       //username and password
-       const newRegistered = await whatsappAdmin.register(newAdmin,password);
-    await newUser.save();
-    res.send(newUser);
-    }
-}
-))
-
-//login using mobile number
+ //login using mobile number
 app.post("/login",async(req,res)=>{
-    const user = await whatsappContact.findOne({ mobile: req.body.mobile });
+    const user = await whatsappAdmin.findOne({ mobile: req.body.mobile });
            
            if (user) {
               
@@ -137,13 +72,120 @@ app.post("/login",async(req,res)=>{
            }})
 
 
-        // Error Handling
-        app.use((err,req,res,next)=>{
-            const {status=500,message="Something went wrong."} = err;
-         
-             res.status(status).send(err);
-         })
+     
 
+//Admin signup
+app.post("/admin/signup",wrapAsync(async(req,res)=>{
+    const {username,mobile,image,password}= req.body;
+
+
+    const userExist = await whatsappAdmin.findOne({mobile:req.body.mobile})
+    if(userExist){
+        res.status(409).send("Number is already used.");
+    }
+    else{
+       
+        const newAdmin = new whatsappAdmin({
+            username,mobile,image
+        })
+ //username and password
+       const newRegistered = await whatsappAdmin.register(newAdmin,password);
+    
+    res.send(newAdmin);
+    }
+}
+))
+
+// display All Contacts
+app.get("/admin/:id",wrapAsync(async(req,res)=>{
+    
+    const userInsideAdmin = await whatsappAdmin.findById(req.params.id).populate("allContacts");
+    // const users = await whatsappContact.find();
+    //      res.send(users);
+  
+    res.send(userInsideAdmin);
+     // res.render("whatsapp/newUser.ejs");
+ }))
+
+// Adding New Contact
+app.post("/admin/:id/newuser", wrapAsync(async (req, res) => {
+    try {
+        const {  mobile } = req.body;
+        const currentAdmin = await whatsappAdmin.findById(req.params.id).populate("allContacts");
+    console.log("current =", currentAdmin.allContacts.mobile);
+        const userExist = await whatsappContact.findOne({ mobile });
+        
+        
+        if (userExist) {
+            console.log("mobile Number is Already Added");
+            return res.status(409).send(userExist);
+        }
+
+        //Data of Existing Admin by mobile
+        const NewUserData = await whatsappAdmin.findOne({mobile});
+        
+        //Data of Current Admin  
+        const Admin = await whatsappAdmin.findById(req.params.id);
+        if (!Admin) {
+            console.error("Mobile Number not found for ID:", req.params.id);
+            return res.status(404).send("Mobile Number not found");
+        }
+        
+        const newUser = new whatsappContact({
+            username:NewUserData.username,
+           image:NewUserData.image,
+           mobile
+        });
+         
+
+    //   await  Admin.allContacts.push(newUser);
+    //     await newUser.save();
+    //     await Admin.save();
+    //     await NewUserData.save();
+
+       
+        res.send("New contact added successfully");
+    } catch (err) {
+        console.error("Error during adding new contact:", err);
+        res.status(500).send("Internal Server Error");
+    }
+}));
+
+// Get All Message
+app.get('/admin/:id/messages',  async (req, res) => {
+ const AllMessages = await whatsappContact.findById(req.params.id).populate("messages");
+ 
+ res.send(AllMessages);
+});
+// Send Message
+app.post('/admin/:id/messages', wrapAsync(
+    async (req, res) => {
+        const { senderMobile,receiverMobile, message } = req.body;
+        const receiverDetail = await whatsappContacts.findOne({ mobile: receiverMobile });
+        const senderDetail = await whatsappContacts.findOne({mobile:senderMobile})
+        if (!receiverDetail) return res.status(400).send('Receiver not found');
+        
+        const newMessage = new whatsappMessage({
+            senderId: req.params.id,
+            receiverId: receiverDetail._id,
+            message,
+        });
+        receiverDetail.messages.push(newMessage);
+        senderDetail.messages.push(newMessage);
+        await receiverDetail.save();
+        await senderDetail.save();
+        await newMessage.save();
+       
+        res.status(201).send(receiverDetail);
+    }
+));
+
+   // Error Handling
+   app.use((err,req,res,next)=>{
+    const {status=500,message="Something went wrong."} = err;
+ 
+     res.status(status).send(err);
+ })
 
 app.listen(8080,()=>{
     console.log("Listening on port 8080");
